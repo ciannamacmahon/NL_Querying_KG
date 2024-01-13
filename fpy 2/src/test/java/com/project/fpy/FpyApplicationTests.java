4d5393ceee715409bb0642b3aad1f1ba7a6866d2
Cianna MacMahon <ciannamacmahon@gmail.com>
2024-01-13 19:55:14 +0000

    new project based off Original python version

---
+++ fpy 2/src/test/java/com/project/fpy/FpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
